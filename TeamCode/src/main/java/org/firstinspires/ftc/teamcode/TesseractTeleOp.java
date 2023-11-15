9d96556f3d4308148df5aa50f0058831a8d3060c
RPHS-Robotics <rockypointhsrobotics@gmail.com>
2023-11-15 15:49:00 -0500

    Setup Motors, Init, & Loop stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesseractTeleOp.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+import java.lang.*;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+@TeleOp(name="Tesseract")
+public class TesseractTeleOp extends OpMode {
+    // Motors
+    public DcMotor frontLeftMotor;
+    public DcMotor frontRightMotor;
+    public DcMotor rearLeftMotor;
+    public DcMotor rearRightMotor;
+    public DcMotor leftCraneMotor;
+    public DcMotor rightCraneMotor;
+    public Gyroscope imu;
+    public DcMotor motorTest;
+
+    @Override
+    public void init() {
+        imu = hardwareMap.get(Gyroscope.class, "imu");
+    }
+
+    @Override
+    public void loop() {
+        double LJoyX = gamepad1.left_stick_x;
+        double LJoyY = gamepad1.left_stick_y;
+        Vector2D LJoyVector = new Vector2D(LJoyX, LJoyY); // Convert Pos to Vector2D
+        double LJoyM = LJoyVector.getNorm(); // Magnitude
+        double LJoyA = 0;
+
+        if (!(LJoyM == 0)) {
+            LJoyA = Vector2D.angle(new Vector2D(0, 1) , LJoyVector); // Angle in Radians
+        } else {
+            LJoyA = 0;
+        }
+
+        frontLeftMotor.setPower(LJoyY); // Reversed Motor
+        frontRightMotor.setPower(LJoyY);
+        rearLeftMotor.setPower(LJoyY); // Reversed Motor
+        rearRightMotor.setPower(LJoyY);
+
+        telemetry.addData("Front Motors: ","FL: %.3f, FR: %.3f",LJoyY, LJoyY);
+        telemetry.addData("Rear Motors", "RL: %.3f, RR: %.3f", LJoyY, LJoyY);
+    }
+}
