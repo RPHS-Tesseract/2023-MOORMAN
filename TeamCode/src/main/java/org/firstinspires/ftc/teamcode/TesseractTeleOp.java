1a61b59af8d4149d30abb41c7c9619ef66837f80
RPHS-Robotics <rockypointhsrobotics@gmail.com>
2023-12-12 15:09:22 -0500

    Added IMU
    Added IMU for future gyroscopic controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesseractTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesseractTeleOp.java
@@ -5,14 +5,17 @@ import java.util.Arrays;
 import java.util.Collections;
 
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.IMU;
 
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.config.TesseractConfig;
 
 @TeleOp(name="Tesseract")
@@ -27,16 +30,19 @@ public class TesseractTeleOp extends OpMode {
     public DcMotor motorFR;
     public DcMotor motorBL;
     public DcMotor motorBR;
+    public DcMotor craneL;
     public DcMotor leftCraneMotor;
     public DcMotor rightCraneMotor;
-    public Gyroscope imu;
+    public IMU imu;
+    public IMU.Parameters imuParams;
     public DcMotor motorTest;
+    public YawPitchRollAngles robotOrientation;
     public double easeTime = TesseractConfig.easeAmount; // Alpha for Joystick Lerp
     public boolean easingEnabled = true; // Disable if causes problems
     // setup joystick variables
-    double LJoyX = 0;
-    double LJoyY = 0;
-    double RJoyX = 0;
+    double LJoyX = 0.0;
+    double LJoyY = 0.0;
+    double RJoyX = 0.0;
     double currentLJX = LJoyX;
     double currentLJY = LJoyY;
     double currentRJX = RJoyX;
@@ -48,10 +54,22 @@ public class TesseractTeleOp extends OpMode {
         motorFL = hardwareMap.get(DcMotor.class, "FL");
         motorBR = hardwareMap.get(DcMotor.class, "BR");
         motorBL = hardwareMap.get(DcMotor.class, "BL");
+        craneL = hardwareMap.get(DcMotor.class, "LCrane");
         motorFL.setDirection(DcMotor.Direction.REVERSE);
         motorFR.setDirection(DcMotor.Direction.FORWARD);
         motorBL.setDirection(DcMotor.Direction.FORWARD);
         motorBR.setDirection(DcMotor.Direction.FORWARD);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        imuParams = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP
+                )
+        );
+        imu.initialize(imuParams);
+        imu.resetYaw();
+
     }
 
     @Override
@@ -63,6 +81,13 @@ public class TesseractTeleOp extends OpMode {
         currentLJY = lerp(currentLJY, LJoyY, easeTime);
         currentLJX = lerp(currentLJX, LJoyX, easeTime);
         currentRJX = lerp(currentRJX, RJoyX, easeTime);
+
+        // IMU Variables
+        robotOrientation = imu.getRobotYawPitchRollAngles();
+        double yaw = robotOrientation.getYaw(AngleUnit.RADIANS);
+        double pitch = robotOrientation.getPitch(AngleUnit.RADIANS);
+        double roll = robotOrientation.getRoll(AngleUnit.RADIANS);
+
 
         double powerFL = 0;
         double powerBL = 0;
@@ -100,9 +125,12 @@ public class TesseractTeleOp extends OpMode {
         motorFR.setPower(powerFR); // Reversed Motor
         motorBL.setPower(powerBL);
         motorBR.setPower(powerBR); // Reversed Motor
+        craneL.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
 
         telemetry.addData("Front Motors: ","FL: %.3f, FR: %.3f",LJoyY, LJoyY);
         telemetry.addData("Rear Motors", "RL: %.3f, RR: %.3f", LJoyY, LJoyY);
         telemetry.addData("Left Joystick", "Raw: %.3f, Mod: %.3f", LJoyY, currentLJY);
+        telemetry.addData("Left Crane", "Raw: %.3f", gamepad1.right_trigger);
+        telemetry.addData("Gyro: ", "Yaw: %.3f, Pitch: %.3f, Roll: %.3f", yaw, pitch, roll);
     }
 }
